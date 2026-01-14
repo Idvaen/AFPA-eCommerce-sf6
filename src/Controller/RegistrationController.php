8d7b2df7b5cea19b0e6c32219a0fe7f0cc1b4442
IvanD <ivan.denysov.v@gmail.com>
2026-01-14 16:39:42 +0100

    petit modifications de Registration et connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $security->login($user, UserAuthenticator::class, 'main');
+            return $security->login($user, UserAuthenticator::class, 'main_index');
         }
 
         return $this->render('registration/register.html.twig', [
